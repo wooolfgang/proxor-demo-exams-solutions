5a17120d7802a1bb1306255bcd83c896e6377aed
Li Arolf Rey <liarolfrrey@gmail.com>
2017-11-26 17:29:02 +0800

    Add exps-have-numbers-ss solution

---
+++ exps-have-numbers-ss/readme.txt
@@ -0,0 +1,19 @@
+readme.txt -- for exps-have-numbers-ss
+
+THE TASK
+
+In this task modify SpreadSheet.java so that numbers can be directly
+entered as expression operands in formulas, freely mixed with cell
+references. For this task, a number is defined as a character string
+that can be interpreted as a double value using Java's
+Scanner::nextDouble() method WITH THE EXCEPTIONS that you may assume
+  1. Operator characters (in particular "+" and "-") are not present,
+     so all numbers are non-negative, and there are no embedded
+     operators, as in 1.3e-6 and 2e+5.
+  2. SignedNonNumbers such as NaN and Infinity are not present.
+
+Video demonstrations of a solution for this task are available.
+    1. During the exam: as exps-have-numbers-ss.html in the "Solution Demonstration Videos"
+       folder on the desktop in the exam delivery environment.
+    2. During exam preparation, outside the exam environment.
+       https://s3-us-west-2.amazonaws.com/proxor-video/video/exps-have-numbers-ss-transcoded.mp4
---
+++ exps-have-numbers-ss/src/ExpsHaveNumbersTest.java
@@ -0,0 +1,43 @@
+// ExpsHaveNumbersTest -- a simple, incomplete test of SpreadSheet,
+// modified to implement the Spreadsheet-exps-have-numbers task
+
+import java.lang.reflect.InvocationTargetException;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class ExpsHaveNumbersTest extends TestCase {
+
+	// this method is designed to run on the AWT thread
+	public void reportError(String msg) {
+		throw new RuntimeException(msg);
+	}
+
+	public final void testEvaluate() {
+		try {
+			javax.swing.SwingUtilities.invokeAndWait(new Runnable() {
+				public void run() {
+					SpreadSheet spreadsheet = new SpreadSheet("SpreadSheet");
+					// Set up the content pane.
+					spreadsheet.addComponentsToPane(spreadsheet
+							.getContentPane());
+					// Display the window.
+					spreadsheet.pack();
+					spreadsheet.setVisible(true);
+					spreadsheet.setCell(0, 0, "123");
+					spreadsheet.setCell(0, 1, "=A1+3");
+					spreadsheet.evaluate();
+					String cellText0 = spreadsheet.getCellText(0, 0);
+					String cellText1 = spreadsheet.getCellText(0, 1);
+					assertTrue("corrupted a cell", cellText0.equals("123"));
+					assertTrue("failed to evaluate formula with 3",
+							Double.valueOf(cellText1) == 126.0);
+				}
+			});
+		} catch (InvocationTargetException e) {
+			Assert.fail(e.getCause().getMessage());
+		} catch (Exception e) {
+			Assert.fail("invokeAndWait (Swing task) raised exception");
+		}
+	}
+}
